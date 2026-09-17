3b51ab53b11ffa9d177562a65ca88037d1d308cd
ARTEA <975998507@qq.com>
2026-09-17 17:58:28 +0800

    Fix workflow and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ARTEAAAA.github.io',
-	base: '/ARTEAAA.github.io/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
